da16a32566424a954a48fcba3bd957662cc18911
AztecSwap <82784291+AztecSwap@users.noreply.github.com>
2021-04-23 16:02:09 -0400

    add bourbon link

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,7 +4,12 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: '/',
+    href: 'https://www.aztecswap.com/',
+  },
+  {
+    label: 'Bourbon DeFi',
+    icon: 'HomeIcon',
+    href: 'https://www.bourbondefi.com/'
   },
   {
     label: 'Trade',
@@ -30,6 +35,24 @@ const config: MenuEntry[] = [
     label: 'Pools',
     icon: 'PoolIcon',
     href: 'https://www.aztecswap.com/pools',
+  },
+  {
+    label: 'Aztec Gold Token',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Dex Guru',
+        href: 'https://dex.guru/token/0x8298fea38aa8627316ffd943fe001b69cac8174d-bsc',
+      },
+      {
+        label: 'Contract',
+        href: 'https://bscscan.com/token/0x8298fea38aa8627316ffd943fe001b69cac8174d#readContract',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0xa040492a50f0034894eb6df0a4d6c8ac33b92cfd',
+      },
+    ],
   },
   // {
   //   label: 'Pools',
